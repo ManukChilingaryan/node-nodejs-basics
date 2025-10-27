81f71cb5a9442401aa0f7b73af64a55105a910e7
chilingaryanmanuk@gmail.com <your_email@example.com>
2025-10-27 18:13:55 +0400

    feaut:add args.js and env.js files

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
+//env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+
 const parseArgs = () => {
   // Write your code here
+  
 };
 
 parseArgs();
+
+console.log(process)
+console.log(process.argv)
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-  // Write your code here
+// Write your code here
+  
+  let result = Object.entries(process.env)
+  .filter(([key]) => key.startsWith("RSS_"))
+  .map(([key,value])=>`${key}=${value}`).join(';')
+
+  console.log(result)
 };
+
+// //MOCK DATA FOR TEST
+// process.env.RSS_arg1 = "value1"
+// process.env.arg2 = "value2"
+// process.env.RSS_arg3 = "value3"
 
 parseEnv();
